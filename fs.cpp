18bdd9def6dd587071e511ac958bb66d2bd61a1a
Silvio Molina <molinahuel44@gmail.com>
2021-11-13 00:01:15 -0300

    doing useless things

--- fs.cpp
+++ fs.cpp
@@ -2,8 +2,32 @@
 #include <iostream>
 #include <experimental/algorithm>
 #include <typeinfo>
+#include <fstream>
 
 namespace fs = ghc::filesystem;
+
+void get_size_file(const fs::path& s){
+
+	using std::fstream;
+	using std::string;
+	using std::cout;
+
+	cout << "File size: " << fs::file_size(s) << "\n";
+
+	fstream myfile;
+	string spath = s.u8string();
+
+	myfile.open(spath, fstream::in | fstream::out | fstream::app);
+
+	if(!myfile){
+		cout << "file unidentified";
+	}else{
+		cout << "file identified";
+		myfile << "\nreading from fs.cpp script\n";
+	}
+
+	myfile.close();
+}
 
 void format_viewer(const fs::path& s){
 	if(s.has_extension()){
@@ -24,7 +48,9 @@ int main(){
 	std::cout << "Project path is " << PROJECT_PATH << '\n';
 	std::cout << "Entire path to text file: " << entire << '\n';
 
+	std::cout << "\n\n";
 	format_viewer(entire);
+	get_size_file(entire);
 
 	return 0;
 }
