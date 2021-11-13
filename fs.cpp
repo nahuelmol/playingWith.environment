f8080d3ea65a40b87c4291fed3d01e192822e1b9
Silvio Molina <molinahuel44@gmail.com>
2021-11-12 22:58:04 -0300

    analizing file attributes

--- fs.cpp
+++ fs.cpp
@@ -1,14 +1,30 @@
 #include <filesystem.hpp>
 #include <iostream>
 #include <experimental/algorithm>
+#include <typeinfo>
 
 namespace fs = ghc::filesystem;
 
-int main(){
+void format_viewer(const fs::path& s){
+	if(s.has_extension()){
+		std::cout << "Text file\n";
+		std::cout << "File type: " << s.extension() << "\n";
+	}
+}
 
-	std::cout << "new attempt\n";
-	std::cout << "Current path is " << fs::current_path() << '\n';
-	std::cout << "Temp directory is " << fs::temp_directory_path();
+int main(){
+	fs::path entire;
+
+	fs::path PROJECT_PATH = fs::current_path();
+	fs::path text_file = "\\out\\new_file.txt";
+
+	entire += PROJECT_PATH;
+	entire += text_file;
+
+	std::cout << "Project path is " << PROJECT_PATH << '\n';
+	std::cout << "Entire path to text file: " << entire << '\n';
+
+	format_viewer(entire);
 
 	return 0;
 }
