d1daad843f638e4ead22ff9afd0685866000c942
Silvio Molina <molinahuel44@gmail.com>
2021-11-15 00:16:16 -0300

    just modifying batch file

--- run.bat
+++ run.bat
@@ -5,8 +5,16 @@ SET BOOST_PATH=D:/mingw_libs/boost
 SET PROJC_PATH=%~dp0
 
 ECHO ..working in %PROJC_PATH%
-ECHO ..compiling main script
+
+GOTO Main
+
+:Fs
+ECHO ..compiling fs script
 c++ -I %BOOST_PATH% -o %OUT% fs.cpp 
+
+:Main
+ECHO ..compiling main script
+c++ -I %BOOST_PATH% -o %OUT% main.cpp
 
 ECHO ..executing 
 cd out && fs
